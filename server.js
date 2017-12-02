32ac321cce6cd7c28b52983a4a3d9712942df6d7
Marcio <marcio.hsdl@gmail.com>
2017-12-02 00:36:07 -0300

    test cors implementation

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,15 +31,51 @@ app.use('/', ocurrences);
 app.use('/', ocurrenceTypes);
 app.use('/', login);
 
+app.all('*', function (req, res, next) {
+
+  // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+  if (req.method === 'OPTIONS') {
+    // CORS Preflight
+    res.send();
+  } else {
+    var targetURL = req.header('Target-URL');
+    if (!targetURL) {
+      res.send(500, {
+        error: 'There is no Target-Endpoint header in the request'
+      });
+      return;
+    }
+    request({
+        url: targetURL + req.url,
+        method: req.method,
+        json: req.body,
+        headers: {
+          'Authorization': req.header('Authorization')
+        }
+      },
+      function (error, response, body) {
+        if (error) {
+          console.error('error: ' + response.statusCode)
+        }
+        //                console.log(body);
+      }).pipe(res);
+  }
+});
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

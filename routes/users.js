dbb5fd69ff0e20171717c152ec5c322b12d50b15
Onnion <marcio.hsdl@gmail.com>
2017-12-18 08:39:07 -0300

    :bug:  router users

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,9 @@ var db = require('../queries');
 router.get('/api/users', db.getAllUsers);
 router.get('/api/users/:codigo_usuario', db.getSingleUser);
 router.get('/api/users/validate/login/:user_deviceJSON', db.verifyLogin);
+router.get('/api/users/validate/user/:userJSON', db.verifyUser);
 router.put('/api/users/login/:device', db.login);
 router.put('/api/users/logout/:device', db.logout);
-router.post('/api/users/validate/user/:userJSON', db.verifyUser);
+router.post('/api/users', db.createUser);
 
 module.exports = router;

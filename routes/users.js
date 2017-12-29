a89dcfa1cec080727733d0c6b34ad3e8b47b5ad7
jmontejr <josecmontejr@gmail.com>
2017-12-28 23:02:21 -0200

    Criar rota para editar usuario

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.get('/api/users', db.getAllUsers);
 router.get('/api/users/:codigo_usuario', db.getSingleUser);
 router.get('/api/users/validate/login/:user_deviceJSON', db.verifyLogin);
 router.get('/api/users/validate/user/:userJSON', db.verifyUser);
+router.put('/api/users/update/:codigo_usuario', db.updateUser);
 router.put('/api/users/login/:device', db.login);
 router.put('/api/users/logout/:device', db.logout);
 router.post('/api/users', db.createUser);
